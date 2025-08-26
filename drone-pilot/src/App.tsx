cc23e3edbe6303ed0331b9dcce7ba2f73050d384
rahmehAlOmary <rahmeh@nadsoft.net>
2025-08-26 13:53:26 +0300

    Project scaffold: added folders, DroneContext, and placeholder components

---
+++ drone-pilot/src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+function App() {
+
+  return (
+    <>
+ <div className="min-h-screen flex items-center justify-center bg-red-500 text-white">
+      <h1 className="text-5xl font-bold underline decoration-blue-500">
+      Sager Pilot Network!
+      </h1>
+    </div>
+    </>
+  )
+}
+
+export default App
